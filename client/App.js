1329a423b30f40a1a2ee858d641842872168fa15
kunman93 <kun.manu93@gmail.com>
2022-10-13 14:52:59 +0200

    import FuzzyBubblesRegular font

--- client/App.js
+++ client/App.js
@@ -1,6 +1,15 @@
 import TodoApp from './components/TodoApp'
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  const [loadedFont] = useFonts({
+   'FuzzyBubblesRegular': require('./assets/fonts/FuzzyBubbles-Regular.ttf')
+  });
+
+  if (!loadedFont) {
+    return null;
+  }
+
   return (
     <TodoApp />
   )
